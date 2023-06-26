50306fd73a05cb5e0dbb5aa2362280c4c855e62a
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-06-26 15:30:44 +1000

    userRoutes and thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// Route for getting all thoughts
+router.get('/', getAllThoughts);
+
+// Route for getting a thought by ID
+router.get('/:thoughtId', getThoughtById);
+
+// Route for creating a new thought
+router.post('/', createThought);
+
+// Route for updating a thought by ID
+router.put('/:thoughtId', updateThought);
+
+// Route for deleting a thought by ID
+router.delete('/:thoughtId', deleteThought);
+
+// Route for adding a reaction to a thought
+router.post('/:thoughtId/reactions', addReaction);
+
+// Route for deleting a reaction from a thought
+router.delete('/:thoughtId/reactions/:reactionId', deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// Route for getting all users
+router.get('/', getAllUsers);
+
+// Route for getting a user by ID
+router.get('/:userId', getUserById);
+
+// Route for creating a new user
+router.post('/', createUser);
+
+// Route for updating a user by ID
+router.put('/:userId', updateUser);
+
+// Route for deleting a user by ID
+router.delete('/:userId', deleteUser);
+
+// Route for adding a friend to a user's friend list
+router.post('/:userId/friends/:friendId', addFriend);
+
+// Route for removing a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', deleteFriend);
+
+module.exports = router;
