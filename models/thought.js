7e4bb3b4ed0c78f3a0256b385862530db2c07e87
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-06-26 13:00:56 +1000

    added comment for user and thought

--- models/thought.js
+++ models/thought.js
@@ -2,68 +2,72 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+// Define the reaction subdocument schema
 const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: () => new mongoose.Types.ObjectId()
+    {
+        reactionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId() // Generate a new ObjectId as the default value
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280 // Set maximum character length for reaction body
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp) // Use a getter method to format the timestamp on query
+        }
     },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => dateFormat(timestamp)
-    }
-  },
   {
     toJSON: {
-      getters: true
+        getters: true // Include getters when converting to JSON
     },
-    id: false
+    id: false // Disable the "_id" field in the subdocument
   }
 );
 
+// Define the thought schema
 const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 280
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1, // Set minimum character length for thought text
+            maxlength: 280 // Set maximum character length for thought text
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp) // Use a getter method to format the timestamp on query
+        },
+        username: {
+            type: String,
+            required: true
+            },
+        reactions: [reactionSchema] // Embed the reaction subdocuments within the thought document as an array
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: timestamp => dateFormat(timestamp)
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    reactions: [reactionSchema]
-  },
   {
     toJSON: {
-      virtuals: true,
-      getters: true
+        virtuals: true, // Include virtual fields when converting to JSON
+        getters: true // Include getters when converting to JSON
     },
-    id: false
+    id: false // Disable the "_id" field in the document
   }
 );
 
+// Define a virtual field to calculate the number of reactions
 thoughtSchema.virtual('reactionCount')
     .get(function () {
     return this.reactions.length;
 });
 
+// Create the Thought model using the thought schema
 const Thought = mongoose.model('Thought', thoughtSchema);
 
 module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -2,43 +2,47 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+// Define the user schema
 const userSchema = new Schema(
     {
         username: {
             type: String,
             required: true,
             unique: true,
-            trim: true
+            trim: true // Remove whitespace from the beginning and end of the string
         },
         email: {
             type: String,
             required: true,
             unique: true,
-            match: [/.+\@.+\..+/, 'Please enter a valid email address']
+            match: [/.+\@.+\..+/, 'Please enter a valid email address'] // Use a regex pattern to validate the email format
         },
         thoughts: [{
             type: Schema.Types.ObjectId,
-            ref: 'Thought'
+            ref: 'Thought' // Reference the Thought model for the thoughts field
         }],
         friends: [{
             type: Schema.Types.ObjectId,
-            ref: 'User'
+            ref: 'User' // Self-reference to the User model for the friends field
         }]
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
+  {
+    toJSON: {
+        virtuals: true, // Include virtual fields when converting to JSON
+        getters: true // Include getters when converting to JSON
+    },
+    id: false // Disable the "_id" field in the document
+  }
 );
 
+// Define a virtual field to calculate the number of friends
 userSchema.virtual('friendCount')
     .get(function () {
     return this.friends.length;
 });
 
+// Create the User model using the user schema
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
